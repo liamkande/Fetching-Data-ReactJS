cd8ac85aa4e076701c087bdaa800d99c4666b3ad
Ansoumane Kande <liam-kande@Ansoumanes-MBP.home>
2017-10-10 19:12:16 -0400

    Saved data to localStorage and added time stamp.

--- src/js/App.js
+++ src/js/App.js
@@ -4,43 +4,51 @@ import Collapsible from './Collapsible'
 
 class App extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            isLoading: false,
-            contacts: []
+  constructor(props){
+      super(props);
+      this.state = {
+          isLoading: true,
+          contacts: []
+      }
+  }
+  componentWillMount() {
+    localStorage.getItem('contacts') && this.setState({
+      contacts: JSON.parse(localStorage.getItem('contacts')),
+      isLoading: false
+    })
+  }
+  componentDidMount(){
+    !localStorage.getItem('contacts') ? this.fetchData()
+    : console.log('using data from localStorage')
+  }
+  componentWillUpdate(nextProps, nextState){
+    localStorage.setItem('contacts', JSON.stringify(nextState.contacts))
+    localStorage.setItem('contactsDate', Date.now())
+  }
+  fetchData(){
+      this.setState({
+          isLoading: true,
+          contacts: []
+      })
+
+      fetch('https://randomuser.me/api/?results=05&nat=us,dk,fr,gb')
+      .then(response => response.json())
+      .then(parsedJSON => parsedJSON.results.map(user => (
+        {
+          name: `${user.name.first} ${user.name.last}`,
+          username: `${user.login.username}`,
+          email: `${user.email}`,
+          location: `${user.location.city}`,
+          picture: `${user.picture.thumbnail}`
         }
-    }
+      )))
+      .then(contacts => this.setState({
+        contacts,
+        isLoading: false
+      }))
 
-    componentWillMount() {
-      this.fetchData()
-
-    }
-
-    fetchData(){
-        this.setState({
-            isLoading: true,
-            contacts: []
-        })
-
-        fetch('https://randomuser.me/api/?results=05&nat=us,dk,fr,gb')
-        .then(response => response.json())
-        .then(parsedJSON => parsedJSON.results.map(user => (
-          {
-            name: `${user.name.first} ${user.name.last}`,
-            username: `${user.login.username}`,
-            email: `${user.email}`,
-            location: `${user.location.city}`,
-            picture: `${user.picture.thumbnail}`
-          }
-        )))
-        .then(contacts => this.setState({
-          contacts,
-          isLoading: false
-        }))
-
-        .catch(error => console.log('parsing failed', error))
-    }
+      .catch(error => console.log('parsing failed', error))
+  }
 
     render() {
       const {isLoading, contacts} = this.state
